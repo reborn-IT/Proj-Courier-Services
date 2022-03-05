6f57d5c3f8d238c08b71e79aee9d14cb55e7959d
Sithum Basnayaka <sithum.dashantha@gmail.com>
2022-03-05 21:02:43 +0530

    Variables & Assets

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.scss';
+
+function Home() {
+  return (
+    <main className="main-wrapper">
+      <div className="container">
+        <div className="home-image"></div>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
