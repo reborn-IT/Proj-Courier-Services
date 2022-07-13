86e7ea3d1359f083a8fdb266b28c6c51cf24ded5
Sithum Dashantha <sithum.dashantha@gmail.com>
2022-07-13 09:50:49 +0530

    .eslint configured

--- src/Components/ReviewCard/index.tsx
+++ src/Components/ReviewCard/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable max-len */
-import React from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 interface IPhotoProperties {
